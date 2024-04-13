85a0163e0a39738b38c9045b5376358763084f30
mynameisphil <166875450+mynameisphil@users.noreply.github.com>
2024-04-13 23:11:09 +0200

    did some minor cleanup

--- Python/day1/day1.py
+++ Python/day1/day1.py
@@ -2,14 +2,14 @@
 https://adventofcode.com/2023/day/1
 What is the sum of all of the calibration values?
 """
-import inspect, os
+import inspect
+import os
 path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 # Using readlines() https://www.geeksforgeeks.org/read-a-file-line-by-line-in-python/
 day1File = open(os.path.join(path, 'day1.txt'), 'r')
 inputList = day1File.readlines()
 
-numbersMap = { 
-              'one' : 1,
+numbersMap = {'one' : 1,
               'two' : 2, 
               'three' : 3,
               'four' : 4,
@@ -17,32 +17,31 @@ numbersMap = {
               'six' : 6,
               'seven' : 7,
               'eight' : 8,
-              'nine' : 9
-              }
-intTotalCalibration = 0
+              'nine' : 9}
+TOTALCALIBRATION = 0
 
-for input in inputList:
-    if len(input) > 0:
+for line in inputList:
+    if len(line) > 0:
         indexList = []
         indexMap = {}
-        for number in numbersMap.keys():
-            index = input.find(number)
+        for number, value in numbersMap.items():
+            index = line.find(number)
             if index >= 0:
-                indexMap[index] = numbersMap[number]
+                indexMap[index] = value
                 indexList.append(index)
-            index = input.rfind(number)
+            index = line.rfind(number)
             if index >= 0:
-                indexMap[index] = numbersMap[number]
+                indexMap[index] = value
                 indexList.append(index)
-        letters = list(input)
+        letters = list(line)
         i = 0
-        while i < len(input):
+        while i < len(line):
             if letters[i].isdigit():
                 indexMap[i] = int(letters[i])
                 indexList.append(i)
                 break
             i += 1
-        i = len(input)-1
+        i = len(line)-1
         while i >= 0:
             if letters[i].isdigit():
                 indexMap[i] = int(letters[i])
@@ -50,7 +49,6 @@ for input in inputList:
                 break
             i -= 1
         indexList.sort()
-        numberAsString = str(indexMap[indexList[0]]) + str(indexMap[indexList[len(indexList)-1]])
-        print(input.rstrip('\n') + ' = ' + numberAsString +' - '  + str(indexMap) + ' - '  + str(indexList) + '\n')
-        intTotalCalibration += int(numberAsString)   
-print(intTotalCalibration)
+        TOTALCALIBRATION += int(str(indexMap[indexList[0]]) + str(indexMap[indexList[len(indexList)-1]]))
+        
+print(TOTALCALIBRATION)
