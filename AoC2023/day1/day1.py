9bd255217406c82ffc24f20e0ae28019c9e9f9f9
mynameisphil <166875450+mynameisphil@users.noreply.github.com>
2024-04-13 18:54:21 +0200

    fixed path and index

--- AoC2023/day1/day1.py
+++ AoC2023/day1/day1.py
@@ -5,9 +5,8 @@ What is the sum of all of the calibration values?
 import inspect, os
 path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 # Using readlines() https://www.geeksforgeeks.org/read-a-file-line-by-line-in-python/
-day1File = open(os.path.join(path, 'day1_test.txt'), 'r')
+day1File = open(os.path.join(path, 'day1.txt'), 'r')
 inputList = day1File.readlines()
-
 intTotalCalibration = 0
 for input in inputList:
     if len(input) > 0:
@@ -20,7 +19,7 @@ for input in inputList:
                 break
             i += 1
         i = len(input)-1
-        while i > 0:
+        while i >= 0:
             if letters[i].isdigit():
                 number += letters[i]
                 break
