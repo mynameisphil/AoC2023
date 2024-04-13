8b4e9995a5f8bad8d8577164d66482429660883f
mynameisphil <166875450+mynameisphil@users.noreply.github.com>
2024-04-13 13:58:58 +0200

    added folder and input to riddle

--- AoC2023/day1.py
+++
@@ -1,5 +0,0 @@
-"""
-https://adventofcode.com/2023/day/1
-What is the sum of all of the calibration values?
-"""
-print("Hello World!")
---
+++ AoC2023/day1/day1.py
@@ -0,0 +1,27 @@
+"""
+https://adventofcode.com/2023/day/1
+What is the sum of all of the calibration values?
+"""
+# Using readlines() https://www.geeksforgeeks.org/read-a-file-line-by-line-in-python/
+day1File = open('day1.txt', 'r')
+inputList = day1File.readlines()
+
+intTotalCalibration = 0
+for input in inputList:
+    if len(input) > 0:
+        number = ''
+        letters = list(input)
+        i = 0
+        while i < len(input):
+            if letters[i].isdigit():
+                number += letters[i]
+                break
+            i += 1
+        i = len(input)-1
+        while i > 0:
+            if letters[i].isdigit():
+                number += letters[i]
+                break
+            i -= 1
+        intTotalCalibration += int(number)   
+print(intTotalCalibration)
