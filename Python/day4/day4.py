8bb2822ef36a89ebea39933c0b57d03e94fdf252
mynameisphil <phillip.rosner@outlook.com>
2024-05-29 23:40:25 +0200

    changed some minor stuff

--- Python/day4/day4.py
+++ Python/day4/day4.py
@@ -12,29 +12,41 @@ def file_reader(file_path):
     return contents
 
 PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-INPUT_FILE_LINES = file_reader(os.path.join(PATH, 'day4.txt'))
+INPUT_FILE_LINES = file_reader(os.path.join(PATH, 'day4_test.txt'))
 REGEX_NUMBERS_ONLY = r"[0-9]+"
+PART1_RESULT = 0
+PART2_RESULT = 0
+LIST_CARDS = []
 
 class Card:
     def __init__(self, inputCard):
-        self.Number = re.findall(REGEX_NUMBERS_ONLY, inputCard.split(':')[0])
+        self.Number = int(re.findall(REGEX_NUMBERS_ONLY, inputCard.split(':')[0])[0])
         self.luckyNumbers = set(map(int, re.findall(REGEX_NUMBERS_ONLY, inputCard.split(':')[1].split('|')[0])))
         self.cardNumbers = set(map(int, re.findall(REGEX_NUMBERS_ONLY, inputCard.split('|')[1])))
         self.winningNumbers = self.cardNumbers.intersection(self.luckyNumbers)
-        self.cardValue = 0
+        self.Value = 0
+        self.copyList = []
         if len(self.winningNumbers) == 1:
-            self.cardValue = 1
+            self.Value = 1
         elif len(self.winningNumbers) > 1:
-            self.cardValue = 1 << len(self.winningNumbers) - 1
+            self.Value = 1 << len(self.winningNumbers) - 1
         
     def __str__(self):
-        return f"{self.cardValue}"
-
-totalWinningPoints = 0       
+        return f"{self.Value}"
 
 for line in INPUT_FILE_LINES:
-    lineCard = Card(line)
-    totalWinningPoints += lineCard.cardValue
-    
-print(totalWinningPoints)
-    
+    card = Card(line)
+    LIST_CARDS.append(card)
+    PART1_RESULT += card.Value
+
+#Solve Part 1
+print("Part 1:",PART1_RESULT)
+
+#Solve Part 2
+print("Part 2:",PART2_RESULT)
+for card in LIST_CARDS: #init copies
+    card.copyList = LIST_CARDS[card.Number:card.Number+len(card.winningNumbers)]
+    copyStr = ""
+    for copy in card.copyList:
+        copyStr += " " + str(copy.Number)
+    print(card.Number, copyStr)
